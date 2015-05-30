83196784d6c34f3d942ea731bb5ddbb0b3661dee
Daniel Rampanelli <hello@neuralquery.com>
2015-05-30 16:03:08 +0200

    Add stub plugin definition file.

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: SOS Cookies
+Plugin URI: http://github.com/neuralquery/soscookies
+Description: SOS Cookies
+Version: 1.0
+Author: Daniel Rampanelli
+Author URI: http://danielrampanelli.com
+Text Domain: soscookies
+Plugin Type: Piklist
+License: GPL, Version 3
+*/
+
+?>
