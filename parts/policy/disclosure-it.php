8d3ad122d70709c6e8c7aff169ee3adbd5ee4fc7
Eva Pavan <eva@articalab.it>
2015-06-01 01:27:40 +0200

    Add italian version of disclosure page.

---
+++ parts/policy/disclosure-it.php
@@ -0,0 +1,47 @@
+<div class="contains-cookie-policy-disclosure">
+    
+    <h3>Informativa ai sensi dell'art. 13 del d.lgs. n. 196/2003 (c.d. codice privacy)</h3>
+    <p><span class="contains-cookie-policy-owner"><?php echo $name ?></span>, con sede in <span class="contains-cookie-policy-address"><?php echo $address ?></span>, informa, ai sensi dell'art. 13 del Codice Privacy ed in ottemperanza alle prescrizioni del Provvedimento 229/2014 del Garante per la Protezione dei Dati Personali, che il presente sito utilizza le seguenti tipologie di cookie (ossia dei piccoli file di testo che i siti visitati inviano al tuo device, dove vengono poi memorizzati al fine di essere ritrasmessi agli stessi siti alla visita successiva):</p>
+
+    <h4>Cookie tecnici</h4>
+    <p>Quei cookie strettamente necessari per permettere:</p>
+    <p>la navigazione e fruizione del sito web;</p>
+    <p>la raccolta di informazioni, in forma aggregata, sul numero degli utenti e su come questi visitano il sito stesso ("cookie analytics");</p>
+    <p>la navigazione in funzione di una serie di criteri selezionati (ad esempio, la lingua, "cookie di funzionalità") al fine di migliorare il servizio reso allo stesso.</p>
+    <p>Tali cookie non vengono utilizzati per scopi ulteriori rispetto a quelli funzionali sopra descritti e pertanto la loro installazione non richiede il tuo consenso.</p>
+
+    <h4>Cookie di profilazione di terze parti</h4>
+    <p>Tali cookie sono installati da soggetti diversi da <span class="contains-cookie-policy-owner"><?php echo $name ?></span> e l'installazione degli stessi richiede il tuo consenso; in mancanza gli stessi non saranno installati.</p>
+    <p>Ti riportiamo quindi di seguito i link alle informative privacy delle terze parti dove potrai esprimere il tuo consenso all'installazione di tali cookie evidenziando che, laddove non effettuassi alcuna scelta e decidessi di proseguire comunque con la navigazione all'interno del presente sito web, acconsentirai all'uso di tali cookie.</p>
+
+    <?php include(__DIR__.'/services.php') ?>
+    
+    <p>In aggiunta a quanto sopra esposto, potrai comunque impostare il tuo browser in maniera da rifiutare automaticamente la ricezione dei cookie attivando l'apposita opzione: il mancato utilizzo dei cookie tecnici, tuttavia, potrebbe comportare difficoltà nell'interazione con il presente sito.</p>
+    
+    <h4>Preferenze privacy</h4>
+    <p>In qualsiasi momento puoi rivedere le impostazioni della privacy per questo sito.</p>
+    
+    <p>
+        <a class="triggers-cookies-preferences" href="#">IMPOSTAZIONI PRIVACY</a>
+    </p>
+    
+    <p>Ti comunichiamo che, ai sensi dell'art. 7 del Codice Privacy, ti spettano determinati diritti, tra i quali quello di opporti al trattamento. A tal fine, pertanto, potrai modificare la tua scelta in qualsiasi momento accedendo nuovamente alla presente informativa.</p>
+    
+    <h3>ISTRUZIONI DISABILITAZIONE COOKIES DAI BROWSERS</h3>
+    
+    <h4>Se si utilizza Internet Explorer</h4>
+    <p>In Internet Explorer, fare clic su "Strumenti" poi "Opzioni Internet". Nella scheda Privacy, spostare il cursore verso l'alto per bloccare tutti i cookie o verso il basso per consentire a tutti i cookie, e quindi fare clic su OK.</p>
+    
+    <h4>Se si utilizza il browser Firefox</h4>
+    <p>Vai al menu "Strumenti" del browser e selezionare il menu "Opzioni" Fare clic sulla scheda "Privacy", deselezionare la casella "Accetta cookie" e fare clic su OK.</p>
+    
+    <h4>Se si utilizza il browser Safari</h4>
+    <p>In Browser della votazione, selezionare il menu "Modifica" e selezionare "Preferences". Clicca su "Privacy". Posizionare l'impostazione "cookies Block" sempre "e fare clic su OK.</p>
+    
+    <h4>Se si utilizza il browser Google Chrome</h4>
+    <p>Fai clic sul menu Chrome nella barra degli strumenti del browser. Selezionare "Impostazioni". Fare clic su "Mostra impostazioni avanzate". Nella sezione "Privacy", fai clic sul pulsante "Impostazioni contenuti". Nella sezione "Cookies", selezionare "Non consentire ai siti per memorizzare i dati" e di controllo "cookie di blocco e di terze parti i dati sito", e quindi fare clic su OK.</p>
+    
+    <h4>Altri browser</h4>
+    <p>Se usi un qualsiasi altro browser, cerca nelle Impostazioni del browser la modalità di gestione dei cookies.</p>
+    
+</div>
