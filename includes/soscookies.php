f17673adccbf2e146e4f42b8781cffd988b94e49
Daniel Rampanelli <hello@neuralquery.com>
2015-05-31 14:53:01 +0200

    Construct the plugin's options from the values configured in the backend.

--- includes/soscookies.php
+++ includes/soscookies.php
@@ -2,26 +2,66 @@
 
 class soscookies {
     
-    public static function enqueueStylesAndScripts() {
-        wp_enqueue_style('soscookies-cookieconsent', plugins_url('assets/cookieconsent.css', __DIR__));
+    public static function option($name, $default = NULL) {
+        $settings = get_option('soscookies_options');
         
-        wp_register_script('soscookies-cookieconsent', plugins_url('assets/cookieconsent.js', __DIR__), array('jquery'), FALSE, TRUE);
+        $value = $default;
+        if (!empty($settings) && array_key_exists($name, $settings)) {
+            $value = $settings[$name];
+        }
         
-        wp_enqueue_script('soscookies', plugins_url('scripts/soscookies.js', __DIR__), array('jquery', 'soscookies-cookieconsent'), FALSE, TRUE);
-        
-        wp_localize_script('soscookies', 'soscookies', array(
-            'options' => (object) array(
-                'cookies' => (object) array(
-                    'analytics' => (object) array(),
-                    'social' => (object) array(),
-                ),
-                'settings' => (object) array(
-                    'consenttype' => 'explicit',
-                    'clickAnyLinkToConsent' => TRUE,
-                    'disableallsites' => TRUE,
-                ),
+        return $value;
+    }
+    
+    private static function getPluginOptions() {
+        $options = (object) array(
+            'cookies' => (object) array(),
+            'settings' => (object) array(
+                'consenttype' => self::option('consent_type', 'explicit'),
+                'clickAnyLinkToConsent' => TRUE,
+                'disableallsites' => TRUE,
+                'tagPosition' => FALSE,
             ),
-        ));
+        );
+        
+        $types = (array) self::option('cookie_types', array());
+        foreach ($types as $type) {
+            $options->cookies->{$type} = (object) array();
+        }
+        
+        $refresh = self::option('refresh_on_consent');
+        $options->settings->refreshOnConsent = !empty($refresh);
+        
+        $options->settings->style = self::option('theme', 'dark');
+        
+        $options->settings->bannerPosition = self::option('banner', 'top');
+        
+        $tag = self::option('tag');
+        if (!empty($tag)) {
+            $options->settings->tagPosition = $tag;
+        }
+        
+        return $options;
+    }
+    
+    public static function enqueueStylesAndScripts() {
+        $status = self::option('status');
+        
+        if (empty($status)) {
+            return;
+        }
+        
+        if (($status == 'testing' && current_user_can('manage_options')) || ($status == 'yes' && !is_user_logged_in())) {
+            wp_enqueue_style('soscookies-cookieconsent', plugins_url('assets/cookieconsent.css', __DIR__));
+            
+            wp_register_script('soscookies-cookieconsent', plugins_url('assets/cookieconsent.js', __DIR__), array('jquery'), FALSE, TRUE);
+            
+            wp_enqueue_script('soscookies', plugins_url('scripts/soscookies.js', __DIR__), array('jquery', 'soscookies-cookieconsent'), FALSE, TRUE);
+            
+            wp_localize_script('soscookies', 'soscookies', array(
+                'options' => self::getPluginOptions()
+            ));
+        }
     }
     
     public static function adminPages($pages) {
