b07f9b8a20572f8267a9400faba81931e0f7708f
Eva Pavan <eva@articalab.it>
2015-06-05 09:28:24 +0200

    Add DE version of cookie disclosure.

---
+++ parts/policy/disclosure-de.php
@@ -0,0 +1,45 @@
+<div class="contains-cookie-policy-disclosure">
+
+    <h3>COOKIES UND DATENSCHUTZ</h3>
+    <p>Die meisten Websites, die Sie besuchen, verwenden Cookies zur Verbesserung des Nutzererlebnisses. Durch diese Cookies kann sich die Website an Sie „erinnern“, sowohl während Ihres Besuchs der Website (mithilfe eines „Session-Cookies“) als auch bei wiederholten Besuchen (mithilfe eines „dauerhaften Cookies“).</p>
+    <p>Cookies werden von dieser Website nur gesetzt, um dem Benutzer bestimmte Funktionen und die Navigation zu ermöglichen oder zu verbessern. Diese Seite verwendet Cookies keinesfalls, um persönliche Daten zu sammeln, zu verarbeiten, zu verbreiten oder zu speichern.</p>
+
+    <h3>COOKIES IM BROWSER BLOCKIEREN (in Englisch)</h3> 
+   <ul>
+        <li><a href="http://windows.microsoft.com/en-US/internet-explorer/delete-manage-cookies#ie=ie-11" target="_blank">Internet Explorer</a></li>
+        <li><a href="http://support.mozilla.org/en-US/kb/Cookies" target="_blank">Firefox</a></li>
+        <li><a href="http://support.google.com/chrome/bin/answer.py?hl=en&amp;answer=95647" target="_blank">Google Chrome</a></li>
+        <li><a href="http://www.opera.com/help/tutorials/security/cookies/" target="_blank">Opera</a></li>
+        <li><a href="https://support.apple.com/kb/PH19214?locale=it_IT&viewlocale=en_US" target="_blank">Safari</a></li>
+        <li><a href="https://support.apple.com/en-us/HT201265" target="_blank">Safari iOS</a></li>
+        <li><a href="http://support.google.com/mobile/bin/answer.py?hl=en&amp;answer=169022" target="_blank">Android</a></li>
+        <li><a href="http://docs.blackberry.com/en/smartphone_users/deliverables/32004/Turn_off_cookies_in_the_browser_60_1072866_11.jsp" target="_blank">Blackberry</a></li>
+        <li><a href="http://www.microsoft.com/windowsphone/en-us/howto/wp7/web/changing-privacy-and-other-browser-settings.aspx" target="_blank">Windows Phone</a></li>
+    </ul>    
+
+    <h3>VERWENDETE COOKIES:</h3>
+    <h4>Technische Cookies</h4>
+    <p>Diese Cookies sind unbedingt für Folgendes erforderlich:</p>
+    <p>Navigation auf der Website bzw. Verwendung der Website (z. B. Möglichkeit Einkäufe zu tätigen oder sich in zugriffsbeschränkten Bereichen anzumelden; "Browsing- oder Sitzungscookies")</p>
+    <p>Sammeln von Informationen in gebündelter Form zur Anzahl der Nutzer und zum Nutzungsverhalten ("Analyse-Cookies")</p>
+    <p>Website-Navigation gemäß bestimmten Auswahlkriterien (z. B. Sprache, Artikel im Warenkorb mittels "Funktions-Cookies") zur Verbesserung des Serviceangebots. </p>
+    <p>Da sie ausschließlich zu den oben genannten Zwecken verwendet werden, ist Ihre Zustimmung nicht erforderlich.</p>
+
+    <h4>Cookies von Drittanbietern</h4>
+    <p>Diese Cookies werden nicht von <?php echo $name ?>, sondern von Dritten installiert. Die Installation dieser Cookies erfordert Ihre Einwilligung; liegt diese nicht vor, werden die Cookies nicht installiert. </p>
+    <p>Zu diesem Zweck stellen wir Ihnen nachstehend die Links zur Datenschutzerklärung dieser Dritten bereit, über die Sie Ihre ausdrückliche Zustimmung zur Installation solcher Cookies erteilen können. Wir weisen Sie darauf hin, dass Sie sich mit der Verwendung solcher Cookies einverstanden erklären, wenn Sie diesbezüglich keine Angaben machen, aber die Website dennoch besuchen.</p>
+
+    <?php include(__DIR__.'/services.php') ?>
+    
+    <h4>Cookie settings</h4>
+    <p>Whenever you want, you can change your privacy settings for this website.</p>
+    
+    <p>
+        <a class="triggers-cookies-preferences" href="#">MANAGE YOUR COOKIES PREFERENCES</a>
+    </p> 
+    <h3>INHABER DER DATENVERARBEITUNG</h3>
+    <p><span class="contains-cookie-policy-owner"><?php echo $name ?></span> - <span class="contains-cookie-policy-address"><?php echo $address ?></span></p>
+    <p>
+        Für Drittanbieter-Cookies gelten die Datenschutzerklärungen der jeweiligen Drittanbieter.
+    </p>
+</div>
