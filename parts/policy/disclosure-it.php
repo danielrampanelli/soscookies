0e7a90ff4deb9ef10560284c324b3d6bfd4b85d9
Eva Pavan <eva@articalab.it>
2015-06-08 23:42:11 +0200

    Fix it version of disclaimer.

--- parts/policy/disclosure-it.php
+++ parts/policy/disclosure-it.php
@@ -1,49 +1,49 @@
 <div class="contains-cookie-policy-disclosure">
 
-    <h3>COOKIE POLICY</h3>
-    <p>Most websites which you visit will use cookies in order to improve your user experience. They enable websites to «remember» you - either for the duration of your visit (using a «session cookie») or for repeat visits (using a «persistent cookie»).</p>
-    <p>This website sets cookies solely in order to enable or enhance functions or improve the navigation experience for the user. Under no circumstances does this website use cookies to collect, process, disseminate or record personal data. If you prefer, it is possible to block some or all cookies, or even delete cookies which have already been set, but you should be aware that you might lose some functions.</p>
+    <h3>INFORMATIVA ESTESA SUI COOKIE</h3>
+    <p>I cookies sono piccoli file di testo che vengono automaticamente posizionati sul PC del navigatore all'interno del browser. Essi contengono informazioni di base sulla navigazione in Internet e grazie al browser vengono riconosciuti ogni volta che l'utente visita il sito.</p>
+    <p>Questo sito utilizza cookie, anche di terze parti, al solo scopo di rendere possibili o di migliorare le funzionalità offerte e la navigazione da parte dell'utente. Questo sito non utilizza in alcun caso i cookie per la raccolta, il trattamento, la diffusione o la registrazione dei dati personali. In qualunque momento è possibile disabilitare i cookies presenti sul browser; ti ricordiamo che quest'opzione potrebbe limitare molte delle funzionalità di navigazione del sito.</p>    
 
-    <h3>HOW TO DISABLE COOKIES IN YOUR BROWSER</h3>
+    <h3>ISTRUZIONI PER LA DISABILITAZIONE COOKIES DAI BROWSERS</h3>
     
-   <ul>
-        <li><a href="http://windows.microsoft.com/en-US/internet-explorer/delete-manage-cookies#ie=ie-11" target="_blank">Internet Explorer</a></li>
-        <li><a href="http://support.mozilla.org/en-US/kb/Cookies" target="_blank">Firefox</a></li>
-        <li><a href="http://support.google.com/chrome/bin/answer.py?hl=en&amp;answer=95647" target="_blank">Google Chrome</a></li>
+    <ul>
+        <li><a href="http://windows.microsoft.com/it-IT/internet-explorer/delete-manage-cookies#ie=ie-11" target="_blank">Internet Explorer</a></li>
+        <li><a href="https://support.mozilla.org/it/kb/Gestione%20dei%20cookie?redirectlocale=en-US&redirectslug=Cookies" target="_blank">Firefox</a></li>
+        <li><a href="https://support.google.com/chrome/answer/95647?hl=it" target="_blank">Google Chrome</a></li>
         <li><a href="http://www.opera.com/help/tutorials/security/cookies/" target="_blank">Opera</a></li>
-        <li><a href="https://support.apple.com/kb/PH19214?locale=it_IT&viewlocale=en_US" target="_blank">Safari</a></li>
-        <li><a href="https://support.apple.com/en-us/HT201265" target="_blank">Safari iOS</a></li>
-        <li><a href="http://support.google.com/mobile/bin/answer.py?hl=en&amp;answer=169022" target="_blank">Android</a></li>
+        <li><a href="https://support.apple.com/kb/PH19214?viewlocale=it_IT&locale=it_IT" target="_blank">Safari</a></li>
+        <li><a href="http://support.apple.com/kb/HT1677" target="_blank">Safari iOS</a></li>
+        <li><a href="https://support.google.com/xoom/answer/169022?hl=it&rd=1" target="_blank">Android</a></li>
         <li><a href="http://docs.blackberry.com/en/smartphone_users/deliverables/32004/Turn_off_cookies_in_the_browser_60_1072866_11.jsp" target="_blank">Blackberry</a></li>
-        <li><a href="http://www.microsoft.com/windowsphone/en-us/howto/wp7/web/changing-privacy-and-other-browser-settings.aspx" target="_blank">Windows Phone</a></li>
-    </ul>    
+        <li><a href="http://www.windowsphone.com/it-IT/how-to/wp7/web/changing-privacy-and-other-browser-settings" target="_blank">Windows Phone</a></li>
+    </ul>
+    <h3>COOKIE UTILIZZATI IN QUESTO SITO</h3>
+    <p><span class="contains-cookie-policy-owner"><?php echo $name ?></span>, con sede in <span class="contains-cookie-policy-address"><?php echo $address ?></span>, informa in ottemperanza alle prescrizioni del Provvedimento 229/2014 del Garante per la Protezione dei Dati Personali, che il presente sito utilizza le seguenti tipologie di cookie:</p>
+    <h4>Cookie tecnici</h4>
+    <p>Quei cookie strettamente necessari per permettere:</p>
+    <p>la navigazione e fruizione del sito web;</p>
+    <p>la raccolta di informazioni, in forma aggregata, sul numero degli utenti e su come questi visitano il sito stesso ("cookie analytics");</p>
+    <p>la navigazione in funzione di una serie di criteri selezionati (ad esempio, la lingua, "cookie di funzionalità") al fine di migliorare il servizio reso allo stesso.</p>
+    <p>Tali cookie non vengono utilizzati per scopi ulteriori rispetto a quelli funzionali sopra descritti e pertanto la loro installazione non richiede il tuo consenso.</p>
 
-    <h3>COOKIES COLLECTED FROM THIS WEBSITE:</h3>
-    <h4>Technical Cookie</h4>
-    <p>Cookies needed to allow:</p>
-    <p>navigation and functionality of website;</p>
-    <p>informations collections of number of users and how do they and how they visit the website ("analytics cookies");</p>
-    <p>browsing function selected criteria (e.g. language, "functionalities cookies") to improve user experience.</p>
-    <p>These cookies are not used for purposes other than those described above and therefore their installation does not require your consent.</p>
-
-    <h4>Information collected by third-parties</h4>
-    <p>We may allow third-parties from other providers that need your consent; if not given they will not be installed.</p>
-    <p>Any third parties to whom we may disclose personal information may have their own privacy policies which describe how they use and disclose personal information. Those policies will govern use, handling and disclosure of your personal information once we have shared it with those third parties as described in this Privacy Policy. If you want to learn more about their privacy practices, we encourage you to visit the websites of those third parties.</p>
+    <h4>Cookie di profilazione di terze parti</h4>
+    <p>Tali cookie sono installati da soggetti diversi da <span class="contains-cookie-policy-owner"><?php echo $name ?></span> e l'installazione degli stessi richiede il tuo consenso in mancanza del quale gli stessi non saranno installati.</p>
+    <p>Ti riportiamo quindi di seguito i link alle informative privacy delle terze parti dove potrai esprimere il tuo consenso all'installazione di tali cookie evidenziando che, laddove non effettuassi alcuna scelta e decidessi di proseguire comunque con la navigazione all'interno del presente sito web, acconsentirai all'uso di tali cookie.</p>
 
     <?php include(__DIR__.'/services.php') ?>
     
-    <h4>Cookie settings</h4>
-    <p>Whenever you want, you can change your privacy settings for this website.</p>
+    <h4>Preferenze sull'utilizzo dei cookie</h4>
+    <p>In qualsiasi momento puoi rivedere le impostazioni per questo sito.</p>
     
     <p>
-        <a class="triggers-cookies-preferences" href="#">MANAGE YOUR COOKIES PREFERENCES</a>
+        <a class="triggers-cookies-preferences" href="#">IMPOSTAZIONI COOKIE</a>
     </p> 
-    <h3>DATA CONTROLLER AND OWNER</h3>
-    <p><span class="contains-cookie-policy-owner"><?php echo $name ?></span> - <span class="contains-cookie-policy-address"><?php echo $address ?></span></p>
+    <h3>TITOLARE DEL TRATTAMENTO DEI DATI</h3>
+    <p>Titolare del trattamento è <span class="contains-cookie-policy-owner"><?php echo $name ?></span> - <span class="contains-cookie-policy-address"><?php echo $address ?></span></p>
     <p>
-        Since the installation of third party Cookies and other tracking systems through the services used within this Application can not be technically controlled by the Owner, any specific references to Cookies and tracking systems installed by third parties are to be considered indicative. In order to obtain complete information, consult the privacy policy for the respective third party services listed in this document.
+        Dal momento che l'installazione di Cookie e di altri sistemi di tracciamento operata da terze parti tramite i servizi utilizzati all'interno di questa Applicazione non può essere tecnicamente controllata dal Titolare, ogni riferimento specifico a Cookie e sistemi di tracciamento installati da terze parti è da considerarsi indicativo. Per ottenere informazioni complete, consulta la privacy policy degli eventuali servizi terzi elencati in questo documento.
     </p>
-    <p> 
-        Given the objective complexity linked to the identification of technologies based on Cookies and their very close integration with the operation of the web, Users are encouraged to contact the Owner should they wish to receive any further information on the use of Cookies themselves and any possible use of them - for example, by a third party - carried out through this site.
+    <p>
+        Vista l'oggettiva complessità legata all'identificazione delle tecnologie basate sui Cookie ed alla loro integrazione molto stretta con il funzionamento del web, l'Utente è invitato a contattare il Titolare qualora volesse ricevere qualunque approfondimento relativo all'utilizzo dei Cookie stessi e ad eventuali utilizzi degli stessi – ad esempio ad opera di terzi – effettuati tramite questo sito.
     </p>
 </div>
