b0536704f481694b9c1046aa3c1992ba7a259bf1
Daniel Rampanelli <hello@neuralquery.com>
2015-09-15 18:54:02 +0200

    Set version to "1.1.0".

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 Plugin Name: SOS Cookies
 Plugin URI: http://github.com/neuralquery/soscookies
 Description: SOS Cookies
-Version: 1.0.4
+Version: 1.1.0
 Author: Daniel Rampanelli
 Author URI: http://danielrampanelli.com
 Text Domain: soscookies
